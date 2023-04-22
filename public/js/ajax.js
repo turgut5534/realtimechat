9a13fbfa394be5ae2bfb4efa8c22737dc6ec883e
turgut5534 <turgutsalgin5534@gmail.com>
2023-04-23 02:58:15 +0300

    fix

---
+++ public/js/ajax.js
@@ -0,0 +1,77 @@
+$.ajaxSetup({
+    beforeSend: function(xhr) {
+        xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
+    }
+});
+
+
+$('.register-form').on('submit', function(e) {
+
+    e.preventDefault()
+
+    $.ajax({
+        type: 'POST',
+        url: $(this).attr('action'),
+        data: $(this).serialize(),
+        success: function(data) {
+
+            iziToast.success({
+                title: 'OK',
+                message: data.message,
+            });
+
+            $('.register-form')[0].reset(); 
+
+            setTimeout(() => {
+                location.href = '/login'
+            }, 1000);
+
+        },
+        error: function(e) {
+
+            var errorMessage = e.responseJSON.message
+
+            iziToast.error({
+                title: 'Error',
+                message: errorMessage,
+            });
+        }
+    })
+
+})
+
+
+$('.login-form').on('submit', function(e) {
+
+    e.preventDefault()
+
+    $.ajax({
+        type: 'POST',
+        url: $(this).attr('action'),
+        data: $(this).serialize(),
+        success: function(data) {
+
+            iziToast.success({
+                title: 'OK',
+                message: data.message,
+            });
+
+            $('.login-form')[0].reset(); 
+
+            setTimeout(() => {
+                location.href = '/login'
+            }, 1000);
+
+        },
+        error: function(e) {
+
+            var errorMessage = e.responseJSON.message
+
+            iziToast.error({
+                title: 'Error',
+                message: errorMessage,
+            });
+        }
+    })
+
+})
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 3000
 const path = require('path')
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
+require('dotenv').config();
+const auth = require('./middlewares/auth')
 
 const User = require('./models/user')
 
@@ -14,7 +18,8 @@ const viewsDirectory = path.join(__dirname, '../templates')
 
 app.use(express.json())
 app.use(express.static(publicDirectory))
-// app.use(cookieParser)
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 app.set('view engine', 'ejs')
 app.set('views', viewsDirectory)
@@ -27,11 +32,86 @@ app.get('/login', (req,res) => {
 
 })
 
+app.post('/login', async(req,res) => {
+
+    try {
+
+        const { email, password } = req.body
+
+        const user = await User.findOne( {where: { email:email } })
+
+        if(!user) {
+            return res.status(400).json({status:false, message: 'Incorrect email or password'})
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password.toString())
+
+        if(!passwordMatch) {
+            return res.status(400).json({status:false, message: 'Incorrect email or password'})
+        }
+
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '6h' })
+
+        res.cookie('token', token, { httpOnly: true });
+
+        res.status(200).json({ message: 'Logged in successfully', user: user })
+
+    } catch(e){
+        console.log(e)
+    }
+
+})
 
 app.get('/register', (req,res) => {
 
     res.render('views/register')
 
+})
+
+app.post('/register', async(req,res) => {
+
+    try {
+
+        const { name, email, password, repassword } = req.body
+
+        if(!name || !email || !password || !repassword) {
+            return res.status(400).json({status: false, message: "Please fill all fields"})
+        }
+
+        if ( password != repassword) {
+            return res.status(400).json({status: false, message: "Password does not match"})
+        }
+
+        const user = await User.findOne({ where: { email: email } })
+
+        if(user) {
+            return res.status(400).json({status: false, message: "A user with this email already exists"})
+        }
+
+        await User.create({
+            name: name,
+            email: email,
+            password: await bcrypt.hash(password, 10)
+        })
+        
+        return res.status(201).json({status: true, message: "You have been registered successfully!"})
+
+    } catch(e) {
+        console.log(e)
+    }
+
+})
+
+app.get('/logout', (req, res) => {
+
+    res.cookie('token', '', { expires: new Date(0) })
+  
+    res.redirect('/login')
+  })
+
+app.get('/me', auth, (req,res) => {
+    
+    res.send(req.user)
 })
 
 server.listen(port, () => {
